115092647b603a650b45070e52e5af0138bf44fe
nilestoomer <user@email.com>
2024-03-18 16:43:35 -0400

    do schemas for movie user review

---
+++ models/Movie.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+let MovieSchema = new Schema({
+    "title": { type: String },
+    "genre": { type: String },
+    "image": { type: String },
+    "description": { type: String },
+    "releaseDate": { type: String },
+    "runtime": { type: String },
+    "budget": { type: String }
+})
+
+export default mongoose.model("movies", MovieSchema)
